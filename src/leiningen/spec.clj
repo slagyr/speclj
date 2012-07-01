4066689a34f18a50dac68fdfee0529a274318053
Colin Jones <trptcolin@gmail.com>
2012-07-01 08:52:36 -0500

    Upgrade to work with lein2
    Things still work just fine with lein1.
    
    Also removed the need to run deps or compile before lein spec on
    either lein version
    
    require in project.clj had to be replaced because of lein2's
    classpath separation.

--- src/leiningen/spec.clj
+++ src/leiningen/spec.clj
@@ -1,7 +1,6 @@
 (ns leiningen.spec
   (:use
-    [clojure.java.io :only [file]]
-    [leiningen.classpath :only [get-classpath-string]])
+    [clojure.java.io :only [file]])
   (:import
     [java.io BufferedInputStream]))
 
@@ -27,8 +26,25 @@
     (.join error-thread 1000)
     (.exitValue process)))
 
+(defn- compute-classpath-string [project]
+  (clojure.string/join java.io.File/pathSeparatorChar
+        ((or (ns-resolve (create-ns 'leiningen.classpath) 'get-classpath)
+             (ns-resolve (create-ns 'leiningen.core.classpath) 'get-classpath))
+          project)))
+
+(defn- prepare [project]
+  (try
+    (require 'leiningen.core.eval)
+    ((ns-resolve 'leiningen.core.eval 'prep)
+       project)
+    (catch Exception e
+      (require 'leiningen.classpath
+               'leiningen.compile)
+      ((ns-resolve 'leiningen.compile 'prep) project false))))
+
 (defn spec [project & args]
+  (prepare project)
   (let [speclj-args (cons "-c" args)
-        classpath (get-classpath-string project)
+        classpath (compute-classpath-string project)
         jvm-args ["-cp" classpath "-Dspeclj.invocation=lein spec"]]
     (java jvm-args "speclj.main" speclj-args (:root project))))
