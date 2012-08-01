5f8223d51a46c23bf90454aba96a37402eac9176
Micah <micahmartin@gmail.com>
2012-08-01 12:42:22 +0200

    lein1 and lein2 options separated

--- project.clj
+++ project.clj
@@ -6,13 +6,18 @@
             :url "file://LICENSE"
             :distribution :repo
             :comments "Copyright 2011-2012 Micah Martin All Rights Reserved."}
+
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [fresh "1.0.2"]
                  [mmargs "1.2.0"]]
   :eval-in-leiningen true
-  :test-path "spec"
-  :test-paths ["spec"]
-  :java-source-path "src"
-  :java-source-paths ["src"]
   :uberjar-exclusions [#"^clojure/.*"]
+
+  ; lein2
+  :test-paths ["spec"]
+  :java-source-paths ["src"]
+
+  ; lein1
+  :test-path "spec"
+  :java-source-path "src"
   )
