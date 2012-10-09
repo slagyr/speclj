91702368fcda8c4886a7f2c0276b633406c210f9
Micah <micahmartin@gmail.com>
2012-10-09 13:56:29 -0500

    fixes calculator_spec.clj example

---
+++ examples/basics/calulator_spec.clj
@@ -0,0 +1,30 @@
+(ns basics-spec.calulator_spec
+  (:use [speclj.core]))
+
+
+(declare ^:dynamic *the-answer*)
+
+(describe "Calculator"
+
+  (before (println "A spec is about to be evaluated"))
+  (after (println "A spec has just been evaluated"))
+  (before-all (println "May the spec'ing begin!"))
+  (after-all (println "That's all folks."))
+  (with nice-format (java.text.DecimalFormat. "0.00000"))
+  (around [it]
+    (binding [*the-answer* 42]
+      (it)))
+
+
+  (it "adds numbers"
+    (should= 2 (+ 1 1)))
+
+  (it "formats numbers nicely"
+    (should= "3.14159" (.format @nice-format Math/PI)))
+
+  (it "knows the answer"
+    (should= 42 *the-answer*))
+  )
+
+
+(run-specs)
