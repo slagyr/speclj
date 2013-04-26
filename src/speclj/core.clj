866865f490eeb72e4e85a6798a03f8280ba434b9
Micah <micahmartin@gmail.com>
2013-04-26 08:10:04 -0500

    removed use of defmethod
    so that line numbers are on failures are correct

--- src/speclj/core.clj
+++ src/speclj/core.clj
@@ -188,47 +188,6 @@
   [form]
   `(should= nil ~form))
 
-(defmulti -should-contain (fn [expected actual] [(type expected) (type actual)]))
-(defmulti -should-not-contain (fn [expected actual] [(type expected) (type actual)]))
-
-(defmethod -should-contain [String String] [seeking within]
-  (when (not (.contains within seeking))
-    (throw (SpecFailure. (str "Expected: " (-to-s seeking) endl "to be in: " (-to-s within) " (using .contains)")))))
-
-(defmethod -should-not-contain [String String] [seeking within]
-  (when (.contains within seeking)
-    (throw (SpecFailure. (str "Expected: " (-to-s seeking) endl "not to be in: " (-to-s within) " (using .contains)")))))
-
-(defmethod -should-contain [Pattern String] [pattern actual]
-  (when (empty? (re-seq pattern actual))
-    (throw (SpecFailure. (str "Expected: " (-to-s actual) endl "to match: " (-to-s pattern) " (using re-seq)")))))
-
-(defmethod -should-not-contain [Pattern String] [pattern actual]
-  (when (not (empty? (re-seq pattern actual)))
-    (throw (SpecFailure. (str "Expected: " (-to-s actual) endl "not to match: " (-to-s pattern) " (using re-seq)")))))
-
-(defmethod -should-contain [Object IPersistentMap] [element coll]
-  (when (not (contains? coll element))
-    (throw (SpecFailure. (str "Expected: " (-to-s element) endl "to be a key in: " (-to-s coll) " (using contains?)")))))
-
-(defmethod -should-not-contain [Object IPersistentMap] [element coll]
-  (when (contains? coll element)
-    (throw (SpecFailure. (str "Expected: " (-to-s element) endl "not to be a key in: " (-to-s coll) " (using contains?)")))))
-
-(defmethod -should-contain [Object IPersistentCollection] [element coll]
-  (when (not (some #(= element %) coll))
-    (throw (SpecFailure. (str "Expected: " (-to-s element) endl "to be in: " (-to-s coll) " (using =)")))))
-
-(defmethod -should-not-contain [Object IPersistentCollection] [element coll]
-  (when (some #(= element %) coll)
-    (throw (SpecFailure. (str "Expected: " (-to-s element) endl "not to be in: " (-to-s coll) " (using =)")))))
-
-(defmethod -should-contain :default [expected actual]
-  (throw (Exception. (str "should-contain doesn't know how to handle these types: " [(type expected) (type actual)]))))
-
-(defmethod -should-not-contain :default [expected actual]
-  (throw (Exception. (str "should-not-contain doesn't know how to handle these types: " [(type expected) (type actual)]))))
-
 (defmacro should-contain
   "Multi-purpose assertion of containment.  Works strings, regular expressions, sequences, and maps.
 
@@ -237,17 +196,51 @@
   (should-contain :foo {:foo :bar})          ; looks for a key in a map
   (should-contain 3 [1 2 3 4])               ; looks for an object in a collection"
   [expected actual]
-  `(-should-contain ~expected ~actual))
+  `(let [expected# ~expected
+         expected-type# (type expected#)
+         actual# ~actual
+         actual-type# (type actual#)]
+     (cond
+       (and (isa? expected-type# String) (isa? actual-type# String))
+       (when (not (.contains actual# expected#))
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "to be in: " (-to-s actual#) " (using .contains)"))))
+       (and (isa? expected-type# Pattern) (isa? actual-type# String))
+       (when (empty? (re-seq expected# actual#))
+         (throw (SpecFailure. (str "Expected: " (-to-s actual#) endl "to match: " (-to-s expected#) " (using re-seq)"))))
+       (isa? actual-type# IPersistentMap)
+       (when (not (contains? actual# expected#))
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "to be a key in: " (-to-s actual#) " (using contains?)"))))
+       (isa? actual-type# IPersistentCollection)
+       (when (not (some #(= expected# %) actual#))
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "to be in: " (-to-s actual#) " (using =)"))))
+       :else (throw (Exception. (str "should-contain doesn't know how to handle these types: " [expected-type# actual-type#]))))))
 
 (defmacro should-not-contain
   "Multi-purpose assertion of non-containment.  See should-contain as an example of opposite behavior."
   [expected actual]
-  `(-should-not-contain ~expected ~actual))
+  `(let [expected# ~expected
+         expected-type# (type expected#)
+         actual# ~actual
+         actual-type# (type actual#)]
+     (cond
+       (and (isa? expected-type# String) (isa? actual-type# String))
+       (when (.contains actual# expected#)
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "not to be in: " (-to-s actual#) " (using .contains)"))))
+       (and (isa? expected-type# Pattern) (isa? actual-type# String))
+       (when (not (empty? (re-seq expected# actual#)))
+         (throw (SpecFailure. (str "Expected: " (-to-s actual#) endl "not to match: " (-to-s expected#) " (using re-seq)"))))
+       (isa? actual-type# IPersistentMap)
+       (when (contains? actual# expected#)
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "not to be a key in: " (-to-s actual#) " (using contains?)"))))
+       (isa? actual-type# IPersistentCollection)
+       (when (some #(= expected# %) actual#)
+         (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "not to be in: " (-to-s actual#) " (using =)"))))
+       :else (throw (Exception. (str "should-not-contain doesn't know how to handle these types: " [expected-type# actual-type#]))))))
 
-(defn coll-includes? [coll item]
+(defn -coll-includes?? [coll item]
   (some #(= % item) coll))
 
-(defn remove-first [coll value]
+(defn -remove-first [coll value]
   (loop [coll coll seen []]
     (if (empty? coll)
       seen
@@ -256,59 +249,58 @@
           (concat seen (rest coll))
           (recur (rest coll) (conj seen f)))))))
 
-(defn coll-difference [coll1 coll2]
+(defn -coll-difference [coll1 coll2]
   (loop [match-with coll1 match-against coll2 diff []]
     (if (empty? match-with)
       diff
       (let [f (first match-with)
             r (rest match-with)]
-        (if (coll-includes? match-against f)
-          (recur r (remove-first match-against f) diff)
+        (if (-coll-includes?? match-against f)
+          (recur r (-remove-first match-against f) diff)
           (recur r match-against (conj diff f)))))))
 
-(defn difference-message [expected actual extra missing]
+(defn -difference-message [expected actual extra missing]
   (str
     "Expected contents: " (-to-s expected) endl
     "              got: " (-to-s actual) endl
     "          missing: " (-to-s missing) endl
     "            extra: " (-to-s extra)))
 
-(defmulti -should== (fn [expected actual] [(type expected) (type actual)]))
-(defmulti -should-not== (fn [expected actual] [(type expected) (type actual)]))
-
-(defmethod -should== [Object Object] [expected actual]
-  (when-not (== expected actual)
-    (throw (SpecFailure. (str "Expected: " (-to-s expected) endl "     got: " (-to-s actual) " (using ==)")))))
-
-(defmethod -should-not== [Object Object] [expected actual]
-  (when-not (not (== expected actual))
-    (throw (SpecFailure. (str " Expected: " (-to-s expected) endl "not to ==: " (-to-s actual) " (using ==)")))))
-
-(defmethod -should== [IPersistentCollection IPersistentCollection] [expected actual]
-  (let [extra (coll-difference actual expected)
-        missing (coll-difference expected actual)]
-    (when-not (and (empty? extra) (empty? missing))
-      (throw (SpecFailure. (difference-message expected actual extra missing))))))
-
-(defmethod -should-not== [IPersistentCollection IPersistentCollection] [expected actual]
-  (let [extra (coll-difference actual expected)
-        missing (coll-difference expected actual)]
-    (when (and (empty? extra) (empty? missing))
-      (throw (SpecFailure. (str "Expected contents: " (-to-s expected) endl "   to differ from: " (-to-s actual)))))))
-
 (defmacro should==
   "Asserts 'equivalency'.
   When passed collections it will check that they have the same contents.
   For anything else it will assert that clojure.core/== returns true."
   [expected actual]
-  `(-should== ~expected ~actual))
+  `(let [expected# ~expected
+         expected-type# (type expected#)
+         actual# ~actual
+         actual-type# (type actual#)]
+     (cond
+       (and (isa? expected-type# IPersistentCollection) (isa? actual-type# IPersistentCollection))
+       (let [extra# (-coll-difference actual# expected#)
+             missing# (-coll-difference expected# actual#)]
+         (when-not (and (empty? extra#) (empty? missing#))
+           (throw (SpecFailure. (-difference-message expected# actual# extra# missing#)))))
+       :else (when-not (== expected# actual#)
+               (throw (SpecFailure. (str "Expected: " (-to-s expected#) endl "     got: " (-to-s actual#) " (using ==)")))))))
 
 (defmacro should-not==
   "Asserts 'non-equivalency'.
   When passed collections it will check that they do NOT have the same contents.
   For anything else it will assert that clojure.core/== returns false."
   [expected actual]
-  `(-should-not== ~expected ~actual))
+  `(let [expected# ~expected
+         expected-type# (type expected#)
+         actual# ~actual
+         actual-type# (type actual#)]
+     (cond
+       (and (isa? expected-type# IPersistentCollection) (isa? actual-type# IPersistentCollection))
+       (let [extra# (-coll-difference actual# expected#)
+             missing# (-coll-difference expected# actual#)]
+         (when (and (empty? extra#) (empty? missing#))
+           (throw (SpecFailure. (str "Expected contents: " (-to-s expected#) endl "   to differ from: " (-to-s actual#))))))
+       :else (when-not (not (== expected# actual#))
+               (throw (SpecFailure. (str " Expected: " (-to-s expected#) endl "not to ==: " (-to-s actual#) " (using ==)")))))))
 
 (defmacro should-not-be-nil
   "Asserts that the form evaluates to a non-nil value"
@@ -401,7 +393,7 @@ are evaluated by evaluation the file as a script.  Optional configuration paramt
 (run-specs :stacktrace true :color false :reporter \"documentation\")"
   (when (identical? (active-runner) @default-runner) ; Solo file run?
     (let [config (apply hash-map configurations)
-          config (merge (dissoc default-config :runner ) config)]
+          config (merge (dissoc default-config :runner) config)]
       (with-bindings (config-mappings config)
         (if-let [filter-msg (describe-filter)]
           (report-message* (active-reporters) filter-msg))
