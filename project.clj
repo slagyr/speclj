2f72a2585265bcc524acb770f5bd6f98e2ac4a7a
Micah <micahmartin@gmail.com>
2013-04-26 17:00:51 -0500

    adding URL to project.clj

--- project.clj
+++ project.clj
@@ -2,22 +2,17 @@
 
 (defproject speclj speclj.version/string
   :description "speclj: Pronounced 'speckle', is a Behavior Driven Development framework for Clojure."
+  :url "http://speclj.com"
   :license {:name "The MIT License"
             :url "file://LICENSE"
             :distribution :repo
-            :comments "Copyright 2011-2012 Micah Martin All Rights Reserved."}
+            :comments "Copyright 2011-2013 Micah Martin All Rights Reserved."}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [fresh "1.0.2"]
                  [mmargs "1.2.0"]]
   :eval-in-leiningen true
   :uberjar-exclusions [#"^clojure/.*"]
-
-  ; lein2
   :test-paths ["spec"]
   :java-source-paths ["src"]
-
-  ; lein1
-  :test-path "spec"
-  :java-source-path "src"
   )
