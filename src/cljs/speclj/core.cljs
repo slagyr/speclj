be04f9d1b098cefe1b33adaca342164e4df4ddd2
Andrew Zures <azures@8thlight.com>
2014-01-14 10:53:27 -0600

    added speclj.run.spec to core.cljs.  this fixes dynamic var warning during cljs testing

--- src/cljs/speclj/core.cljs
+++ src/cljs/speclj/core.cljs
@@ -1,5 +1,6 @@
 (ns speclj.core
   "Speclj's API.  It contains nothing but macros, so that it can be used
   in both Clojure and ClojureScript."
-  (:require [clojure.data]))
+  (:require [clojure.data]
+            [speclj.run.standard]))
 
