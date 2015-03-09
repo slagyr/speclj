97d3a550fea2c4091e2435d8f0be0e6d140d8389
Micah <micahmartin@gmail.com>
2015-03-09 13:58:07 -0500

    adds manual spec command for working on speclj

---
+++ bin/spec.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Sometimes an installed version of speclj will conflict with local runs.
+# java.lang.IllegalArgumentException: No implementation of method: :report-description of protocol: #'speclj.reporting/Reporter found for class: speclj.report.progress.ProgressReporter
+# Try this command when that happens.
+
+java -cp `lein classpath` speclj.main spec/clj target/spec/clj
